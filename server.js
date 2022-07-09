be287bb8cbe38e106fc72d94fb5c2396c9bdf986
Henriqui Ferreira Segantini <henriqui51298@gmail.com>
2022-07-09 12:31:16 -0300

    Quarto commit, consumindo API pelo back-end

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
 const cors = require('cors')
 const express = require('express')
+const axios = require('axios')
+
 const app = express()
 
 app.use(cors())
 
-app.get('/', (req,res) => {
-  return res.json([
-    { nome: 'Henriqui'},
-    { nome: 'Felipe'},
-    { nome: 'Diego'}
-  ])
+app.get('/', async (req,res) => {
+
+  const { data } = await axios('https://jsonplaceholder.typicode.com/users')
+  
+
+  return res.json(data)
 })
 
 app.listen(4567, () => console.log("Servidor ativado na porta 4567") )
