2a8bb22ffc8b18c621fdf558f8927cfdb59f2c15
Henriqui Ferreira Segantini <henriqui51298@gmail.com>
2022-07-09 12:13:06 -0300

    Segundo commit

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
+const cors = require('cors')
 const express = require('express')
 const app = express()
 
+app.use(cors())
+
 app.get('/', (req,res) => {
-  return res.json({ message: 'ok'})
+  return res.json([
+    { nome: 'Henriqui'},
+    { nome: 'Felipe'},
+    { nome: 'Diego'}
+  ])
 })
 
 app.listen(4567, () => console.log("Servidor ativado na porta 4567") )
